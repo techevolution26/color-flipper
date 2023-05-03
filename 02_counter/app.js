2c6f13f58cb0570a9e2efba76a9cebaceff17137
John Kithi <techevo404@gmail.com>
2023-05-04 00:36:33 +0300

    application

---
+++ 02_counter/app.js
@@ -0,0 +1,30 @@
+
+// initial count
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+// setting up functions to the buttons
+btns.forEach( function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    else if (count < 0) {
+      value.style.color = "red";
+    }
+    else if (count === 0) {
+      value.style.color = "black";
+    }
+
+    value.textContent = count;
+  });
+});
